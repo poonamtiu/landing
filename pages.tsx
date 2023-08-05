5ceac0899e63bce1eccadaadc733fc357e305b09
poonamtiu <139611415+poonamtiu@users.noreply.github.com>
2023-08-05 21:10:44 +0530

    pages.tsx

---
+++ pages.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Pages = () => {
+  return (
+    <div className='bg-white'>
+    <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-col text-center w-full mb-20">
+      <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Why You Choose Us</h2>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">We Bring You The World’s Best Courses From All The Top Publishers, Absolutely FREE!</h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Having over thousand students worldwide and more than 500 online courses available.</p>
+    </div>
+    <div className="flex flex-wrap">
+      <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">HIGHEST RATED</h2>
+        <p className="leading-relaxed text-base mb-4">FUTURE FLAME is the most popular institute among the new generation student's for it's course curriculum and unique training methods.As somany student's passing out our certified course and successfully build a professional carrier is a credit'no doubt..</p>
+        <a className="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">LAB $ WI-FI</h2>
+        <p className="leading-relaxed text-base mb-4">With top class infrastructure and the entire premises is wi-fi enable.Student gets extra lab and practical assistance at any given working hour.</p>
+        <a className="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">100% PLACEMENT</h2>
+        <p className="leading-relaxed text-base mb-4">Placement is our strongest point.We have in close aliance with more than 60 IT companies for direct recruitment.Till now we have been achieving 100% placement till date.We have a placement cell any student enroling with us,becomes a permanent candidate for lifelong and can appear for interview anytime he wants.</p>
+        <a className="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+      <div className="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
+        <h2 className="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">SKILLED FACULTY</h2>
+        <p className="leading-relaxed text-base mb-4">No doubt our faculties are the best in the industry.We train our student's in live platform so that they get the best practical exposure to face any competition in the IT world.</p>
+        <a className="text-indigo-500 inline-flex items-center">Learn More
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+    <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn More..</button>
+  </div>
+</section>
+</div>
+  )
+}
+
+export default Pages
