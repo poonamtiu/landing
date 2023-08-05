2823fb60432bf51a21df56a519cf72324ee34651
poonamtiu <139611415+poonamtiu@users.noreply.github.com>
2023-08-05 21:40:44 +0530

    global.tsx

---
+++ global.tsx
@@ -0,0 +1,74 @@
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+
+.banner {
+    background-image: url("/../computing-costs-ai-banner.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    
+}
+
+.about{
+    transition:0.2s ease;
+	border-radius:10px 8px 10px 6px;
+	overflow:hidden;
+}
+
+.about :hover{
+    transform:scale(1.04);
+}
+
+.enroll{
+    background-image: url("/../360_F_434458653_dMDMTN1VpeBxEHuOtlS0esEztiSVVQ9y.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    position: fixed;
+    opacity: 0.9;
+}
+
+
+
+.conta {
+    border:0;
+    allowfullscreen:"";
+    width:600;
+    height:450;
+    referrerpolicy: no-referrer-when-downgrade; 
+}
+
+.foot {
+    background-image: url("/../footer.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+
+.cate {
+    color:  #fff;
+    cursor: pointer;
+}
+
+.navbar {
+    color: rgb(226, 198, 43);
+    cursor: pointer;
+}
+
+.place {
+    color: black;
+    cursor: pointer;
+} 
+
+.subs {
+    color: black;
+    cursor: pointer;
+}
+
+.future {
+    color: black;
+    cursor: pointer;
+}
+
