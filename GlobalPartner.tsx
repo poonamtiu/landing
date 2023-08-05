286a1a84dd2c2d95575a33b297305b74ece54df4
poonamtiu <139611415+poonamtiu@users.noreply.github.com>
2023-08-05 21:14:25 +0530

    GlobalPartner.tsx

---
+++ GlobalPartner.tsx
@@ -0,0 +1,79 @@
+
+import exp from 'constants'
+import React from 'react'
+
+ const GlobalPartner = () => {
+  return (
+
+    <div className='bg-white pt-[80px] pb-[80px] '>
+             <div className='flex items-center justify-center flex-col'>
+             <h1 className='text-32px font-bold text-[#0e1d34]'>Our Global Partners</h1>
+             <div className='h-[4px] w-[40px] mt-[8px] bg-[#0e1d34]'></div>
+        </div>
+      <div className='container mx-auto pt-[80px] pb-[80px]  space-y-[120px]'>
+        
+        <div className='pr-[15%] '>
+          <div className='flex gap-[30px]'>
+          <div className='flex justify-end items-end'>
+               <img src="global patner1.png" className="w-[75%]" alt="global patner1" />
+          </div>
+          <div>
+            <h2 className='text-[26px] leading-[23px] font-semibold mb-[10px]'>Recommendation By Wipro</h2>
+            <p>We commend FutureFLAME IT Institute for its dedication to nurturing talent and equipping students with the necessary skills to excel in the dynamic world of information technology. Your commitment to quality education and hands-on learning experiences has been evident throughout our engagement.</p>
+            <ul className='mt-[10px] space-y-[15px] '>
+              <li>Industry-Relevant Curriculum: Continuously update the curriculum to align with the latest trends and developments in the IT industry. This will ensure that students are equipped with the most relevant and in-demand skills required by employers.</li>
+              <li>Practical Application: While theoretical knowledge is essential, emphasis on practical application through real-world projects and internships will significantly benefit students. Providing exposure to real-life scenarios will boost their confidence and employability.</li>
+              <li>Industry Experts as Faculty: Consider bringing in experienced professionals from the IT industry to teach and mentor students. Industry experts can share valuable insights and bridge the gap between academic learning and real-world challenges.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+           <div className='pl-[10%]'>
+               <div className='flex gap-[20px]'>
+                   <div className='flex justify-start items-start flex-col pt-[20px]'>
+                   <h2 className='text-[26px] leading-[23px] font-semibold mb-[10px]'>Endrosement By Tech Mahindra</h2>
+            <p>We must commend FutureFLAME for its well-structured curriculum and knowledgeable instructors. The course content was comprehensive, covering a wide range of relevant topics in the IT industry</p>
+            <p className='mt-[10px] w-[700px] '>Implement a robust feedback mechanism where students can share their thoughts on courses, faculty, and the overall learning experience. Regular feedback assessment will help identify areas for improvement. </p>
+                  </div>
+                  <div>
+                    <img src="global patner2.png" className='w-[75%]' alt="global patner2" />
+                  </div>
+               </div>
+           </div>
+
+           <div className='pr-[15%] '>
+          <div className='flex gap-[30px]'>
+          <div className='flex justify-end items-end'>
+               <img src="global patner3.png" className="w-[75%]" alt="global patner3" />
+          </div>
+          <div>
+            <h2 className='text-[26px] leading-[23px] font-semibold mb-[10px]'>Approbation By Dell</h2>
+            <p>We would like to extend our sincerest gratitude for the opportunity to collaborate with your esteemed institution and participate in the recent IT training program. We believe that nurturing young talent and fostering their skills is crucial for the future of the IT industry, and we commend the efforts of Future Flame in this regard.</p>
+            <ul className='mt-[10px] space-y-[15px] '>
+              <li>Technology Infrastructure: Invest in modern infrastructure and the latest technology tools to create a conducive learning environment for students. Access to state-of-the-art equipment and software will enhance their learning experience.</li>
+              <li>Soft Skills Development: Alongside technical skills, focus on nurturing soft skills such as communication, teamwork, and problem-solving. These abilities are crucial for students to excel in their careers and work effectively in diverse teams.</li>
+              <li>Industry Internships and Placements: Strengthen ties with leading companies in the IT sector to facilitate internships and placement opportunities for your students. Practical exposure in professional settings will help students transition smoothly into the corporate world.</li>
+              </ul>
+          </div>
+        </div>
+      </div>
+           <div className='pl-[10%]'>
+               <div className='flex gap-[20px]'>
+                   <div className='flex justify-start items-start flex-col pt-[20px]'>
+                   <h2 className='text-[26px] leading-[23px] font-semibold mb-[10px]'>Commendation By Hyundai</h2>
+            <p>Future Flame aims to empower aspiring IT professionals with cutting-edge knowledge and skills. </p>
+            <p className='mt-[10px] w-[700px] '>Encourage faculty members to engage in continuous learning and professional development to stay updated with industry trends and best practices. </p>
+                  </div>
+                  <div>
+                    <img src="global patner4.png" className='w-[75%]' alt="global patner4" />
+                  </div>
+               </div>
+           </div>
+           </div>
+      </div>
+
+
+    
+  )
+}
+export default GlobalPartner
