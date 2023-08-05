df0bb88c845e3e697d3f808963781946feb45b7d
poonamtiu <139611415+poonamtiu@users.noreply.github.com>
2023-08-05 21:12:39 +0530

    enroll.tsx

---
+++ enroll.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+ const Enroll = () => {
+  return (
+    <div className='h-[40vh]'>
+      <div className='w-full h-[80%] enroll fixed top-[0%] left-0 z-[-10] enroll ' >
+     <div className='w-full h-[100%] flex justify-center items-center'>
+        <div className='text-white flex items-center justify-center flex-col space-y[15x]'>
+            <h1 className='text-[24px] font-bold'>We provide universal access to the world’s best education & offer courses for anyone, Absolutely FREE.</h1>
+            <p>We continuously keep track record of the jobs in the market.</p>
+            <button className=' border-white border-[1.5px] py-]10px] px-[20px] rounded-[4px]'>Enroll Now</button>
+        </div>
+     </div>
+     </div>
+    </div>
+  )
+}
+export default Enroll
